017f0a6eae1099aae7d025637f8050bad952c0cb
lext <aleksei.tiulpin@protonmail.ch>
2018-10-08 20:05:12 +0300

    Project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mctseg',
+    version='0.1',
+    author='Aleksei Tiulpin',
+    author_email='aleksei.tiulpin@oulu.fi',
+    packages=find_packages(),
+    include_package_data=True,
+    license='LICENSE',
+    long_description=open('README.md').read(),
+)
