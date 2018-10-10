9d59ab60d2a24e691b87c6ead484d20e3f719f56
lext <aleksei.tiulpin@protonmail.ch>
2018-10-10 11:28:01 +0300

    Datasets are initialized in the main loop

--- mctseg/unet/train.py
+++ mctseg/unet/train.py
@@ -24,7 +24,7 @@ import torch.nn as nn
 from mctseg.unet.session import init_session
 from mctseg.unet.metadata import init_metadata
 from mctseg.unet.model import UNet
-from mctseg.unet.dataset import init_data_processing, SegmentationDataset
+from mctseg.unet.dataset import init_data_processing, SegmentationDataset, read_gs_ocv
 from mctseg.unet.loss import BCEWithLogitsLoss2d, BinaryDiceLoss, CombinedLoss
 from mctseg.utils import GlobalKVS
 
@@ -46,14 +46,23 @@ if __name__ == "__main__":
                    center_depth=kvs['args'].cdepth,
                    n_inputs=kvs['args'].n_inputs,
                    n_classes=kvs['args'].n_classes,
-                   activation='relu'
-                   )
+                   activation='relu')
         if kvs['gpus'] > 1:
             net = nn.DataParallel(net).to('cuda')
 
         net = net.to('cuda')
 
+        X_train = kvs['metadata'].iloc[train_idx]
+        X_val = kvs['metadata'].iloc[train_idx]
+        train_dataset = SegmentationDataset(split=X_train,
+                                            trf=kvs['train_trf'],
+                                            read_img=read_gs_ocv,
+                                            read_mask=read_gs_ocv)
 
+        val_dataset = SegmentationDataset(split=X_val,
+                                            trf=kvs['val_trf'],
+                                            read_img=read_gs_ocv,
+                                            read_mask=read_gs_ocv)
 
 
 
