a9680ac03bbcd3acec249243abb6796a3097c6d0
lext <aleksei.tiulpin@protonmail.ch>
2018-10-10 11:15:35 +0300

    Able to pickle transforms

--- mctseg/unet/dataset.py
+++ mctseg/unet/dataset.py
@@ -10,6 +10,8 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torchvision import transforms
 from mctseg.utils import GlobalKVS
+import copy
+from functools import partial
 
 
 class SegmentationDataset(data.Dataset):
@@ -37,10 +39,10 @@ class SegmentationDataset(data.Dataset):
 
 def init_data_processing():
     kvs = GlobalKVS()
-    train_ppl = init_train_augmentation_pipeline()
+    train_augs = init_train_augmentation_pipeline()
 
     dataset = SegmentationDataset(split=kvs['metadata'],
-                                  trf=train_ppl,
+                                  trf=train_augs,
                                   read_img=read_gs_ocv,
                                   read_mask=read_gs_ocv)
 
@@ -50,11 +52,26 @@ def init_data_processing():
                                                            n_threads=kvs['args'].n_threads,
                                                            n_classes=kvs['args'].n_classes+1)
 
+    norm_trf = transforms.Normalize(torch.from_numpy(mean_vector).float(), torch.from_numpy(std_vector).float())
+    train_trf = transforms.Compose([
+        train_augs,
+        partial(apply_by_index, transform=norm_trf, idx=0)
+    ])
+
+    val_trf = transforms.Compose([
+        partial(apply_by_index, transform=gs2tens, idx=[0, 1]),
+        partial(apply_by_index, transform=norm_trf, idx=0)
+    ])
+
+    kvs.update('train_trf', train_trf)
+    kvs.update('val_trf', val_trf)
+    kvs.save_pkl(os.path.join(kvs['args'].snapshots, kvs['snapshot_name'], 'session.pkl'))
+
 
 def init_train_augmentation_pipeline():
     kvs = GlobalKVS()
     ppl = transforms.Compose([
-        lambda x: img_mask2solt(*x),
+        img_mask2solt,
         slc.Stream([
             slt.RandomFlip(axis=1, p=0.5),
             slc.SelectiveStream([
@@ -72,8 +89,7 @@ def init_train_augmentation_pipeline():
             slt.CropTransform(crop_size=(kvs['args'].crop_x, kvs['args'].crop_y), crop_mode='r')
         ]),
         solt2img_mask,
-        lambda x: (gs2tens(x[0]), gs2tens(x[1]))
-
+        partial(apply_by_index, transform=gs2tens, idx=[0, 1]),
     ])
     return ppl
 
@@ -87,7 +103,8 @@ def gs2tens(x, dtype='f'):
         raise NotImplementedError
 
 
-def img_mask2solt(img, mask):
+def img_mask2solt(imgmask):
+    img, mask = imgmask
     if len(img.shape) == 2:
         img = img.reshape(img.shape[0], img.shape[1], 1)
     return sld.DataContainer((img, mask.squeeze()), 'IM')
@@ -138,3 +155,35 @@ def init_mean_std(snapshots_dir, dataset, batch_size, n_threads, n_classes):
                                                                       class_weights.astype(np.float32)])
 
     return mean_vector, std_vector, class_weights
+
+
+def apply_by_index(items, transform, idx=0):
+    """Applies callable to certain objects in iterable using given indices.
+    Parameters
+    ----------
+    items: tuple or list
+    transform: callable
+    idx: int or tuple or or list None
+    Returns
+    -------
+    result: tuple
+    """
+    if idx is None:
+        return items
+    if not isinstance(items, (tuple, list)):
+        raise TypeError
+    if not isinstance(idx, (int, tuple, list)):
+        raise TypeError
+
+    if isinstance(idx, int):
+        idx = [idx, ]
+
+    idx = set(idx)
+    res = []
+    for i, item in enumerate(items):
+        if i in idx:
+            res.append(transform(item))
+        else:
+            res.append(copy.deepcopy(item))
+
+    return tuple(res)
