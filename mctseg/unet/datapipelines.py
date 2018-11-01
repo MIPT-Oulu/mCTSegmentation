11d3ddc6c533917b3148e9eb4ef6ce7142f2bc53
lext <aleksei.tiulpin@protonmail.ch>
2018-11-01 09:23:47 +0200

    Better default hyperparameters and augmentations

--- mctseg/unet/datapipelines.py
+++ mctseg/unet/datapipelines.py
@@ -12,17 +12,7 @@ def build_train_augmentation_pipeline():
         img_mask2solt,
         slc.Stream([
             slt.RandomFlip(axis=1, p=0.5),
-            slc.SelectiveStream([
-                slc.Stream([
-                    slt.RandomRotate(rotation_range=(-10, 10), p=1),
-                    slt.RandomShear(range_x=(-0.1, 0.1), p=0.5),
-                    slt.RandomShear(range_y=(-0.1, 0.1), p=0.5),
-                    slt.RandomScale(range_x=(0.9, 1.2), same=True, p=1),
-                    slt.ImageGammaCorrection(gamma_range=(0.8, 1.3))
-                ]),
-                slc.Stream(),
-                slt.ImageGammaCorrection(gamma_range=(0.8, 1.3)),
-            ]),
+            slt.ImageGammaCorrection(gamma_range=(0.5, 2), p=0.5),
             slt.PadTransform(pad_to=(kvs['args'].crop_x+1, kvs['args'].crop_y+1)),
             slt.CropTransform(crop_size=(kvs['args'].crop_x, kvs['args'].crop_y), crop_mode='r')
         ]),
