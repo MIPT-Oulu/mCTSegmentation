1b3348bb75179917e92b9cea4ad93be7292cabd1
lext <aleksei.tiulpin@protonmail.ch>
2019-05-16 09:19:19 +0300

    fixed OOF, changed to deeppipeline

--- scripts/oof_inference.py
+++ scripts/oof_inference.py
@@ -1,24 +1,16 @@
 import cv2
-import sys
 import pickle
 import argparse
 import os
-import glob
-import numpy as np
-
-from termcolor import colored
-import torch
-from torch.utils.data import DataLoader
-from torch.utils.data.sampler import SequentialSampler
-from tqdm import tqdm
 
 
-from mctseg.dataset import SegmentationDataset
+from deeppipeline.kvs import GlobalKVS
+from deeppipeline.io import read_gs_binary_mask_ocv, read_gs_ocv
+from deeppipeline.segmentation.evaluation import run_oof_binary
 
 cv2.ocl.setUseOpenCL(False)
 cv2.setNumThreads(0)
 
-DEBUG = sys.gettrace() is not None
 
 if __name__ == "__main__":
     kvs = GlobalKVS(None)
@@ -41,56 +33,7 @@ if __name__ == "__main__":
         args.depth = session_backup['args'][0].depth
         args.cdepth = session_backup['args'][0].cdepth
         args.seed = session_backup['args'][0].seed
-        # For compatibility with the old arguments
-        args.model = 'unet' if session_backup['args'][0].model == 'training' else session_backup['args'][0].model
 
     kvs.update('args', args)
-
-    metadata = session_backup[f'metadata'][0]
-    for sample_name, _ in metadata.groupby(by='sample_id'):
-        os.makedirs(os.path.join(args.snapshots_root, args.snapshot, 'oof_inference', sample_name), exist_ok=True)
-
-    predicts = []
-    fnames = []
-    gt = []
-    for fold_id, _, val_set in session_backup['cv_split'][0]:
-        print(colored('====> ', 'green') + f'Loading fold [{fold_id}]')
-        snapshot_name = glob.glob(os.path.join(args.snapshots_root, args.snapshot, f'fold_{fold_id}*.pth'))
-        if len(snapshot_name) == 0:
-            continue
-        snapshot_name = snapshot_name[0]
-
-        net = session.init_model(ignore_data_parallel=True)
-        net.load_state_dict(torch.load(snapshot_name))
-
-        if torch.cuda.device_count() > 1:
-            net = torch.nn.DataParallel(net).to('cuda')
-        net.eval()
-
-        if args.tta:
-            raise NotImplementedError('TTA has not yet been implemented')
-        else:
-            test_trf = session_backup['val_trf'][0]
-
-        val_dataset = SegmentationDataset(split=val_set,
-                                          trf=session_backup['val_trf'][0],
-                                          read_img=read_gs_ocv,
-                                          read_mask=read_gs_mask_ocv)
-        val_loader = DataLoader(val_dataset, batch_size=args.bs,
-                                num_workers=args.n_threads,
-                                sampler=SequentialSampler(val_dataset))
-
-        with torch.no_grad():
-            for batch in tqdm(val_loader, total=len(val_loader), desc=f'Predicting fold {fold_id}:'):
-                img = batch['img']
-                sample_ids = batch['sample_id']
-                fnames = batch['fname']
-                predicts = torch.sigmoid(net(img)).mul(255).to('cpu').numpy().astype(np.uint8)
-
-                for idx, fname in enumerate(fnames):
-                    pred_mask = predicts[idx].squeeze()
-                    cv2.imwrite(os.path.join(args.snapshots_root,
-                                             args.snapshot,
-                                             'oof_inference', sample_ids[idx], fname), pred_mask)
-
-
+    run_oof_binary(session_backup=session_backup, read_img=read_gs_ocv,
+                   read_mask=read_gs_binary_mask_ocv, img_group_id_colname='sample_id')
